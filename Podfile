11a0d91ee101d9a50bc79b6f43184558491949e3
acne <ehfkswl@gmail.com>
2019-02-18 19:10:57 +0900

    send image to sever and get result in mobile

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'image_preprocessing_example' do
+  use_frameworks!
+    pod 'Socket.IO-Client-Swift', '~> 13.2.0'
+    pod 'Starscream', '<= 3.0.5'
+end
+
+
--- image_preprocessing_example/ViewController.swift
+++ image_preprocessing_example/ViewController.swift
@@ -7,19 +7,118 @@
 //
 
 import UIKit
+import SocketIO
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    let picker = UIImagePickerController()
+    let manager = SocketManager(socketURL: URL(string: "http://166.104.248.216:5000")!, config: [.log(true), .compress])
+    
+    lazy var socket = manager.defaultSocket
 
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self
+        initializeCaptureSession()
+        socket.on("result") {data, ack in
+            print(data[0])
+            self.result.text = data[0] as? String
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func initializeCaptureSession(){
+        let captureSession = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        captureSession.addInput(input)
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+     }
+    /*
+        session.sessionPreset = AVCaptureSession.Preset.high
+        camera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let cameraCaptureInput = try AVCaptureDeviceInput(device: camera!)
+            cameraCaptureOutput = AVCapturePhotoOutput()
+            
+            session.addInput(cameraCaptureInput)
+            session.addOutput(cameraCaptureOutput!)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.bounds
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        
+        view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+        
+        session.startRunning()
     }
-
-
+    */
+    
+    
+    @IBOutlet weak var result: UITextView!
+    @IBOutlet weak var imageVIew: UIImageView!
+    @IBAction func addAction(_ sender: Any) {
+        let alert =  UIAlertController(title: "원하는 타이틀", message: "원하는 메세지", preferredStyle: .actionSheet)
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in
+            self.openLibrary()
+        }
+        
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in
+            self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func connect(_ sender: Any) {
+        socket.connect()
+    }
+    
+    @IBAction func message(_ sender: Any) {
+        socket.emit("test", "test message")
+    }
+    
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        }
+        else{
+            print("Camera not available")
+        }
+    }
 }
 
+
+
+extension ViewController : UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[String: Any]){
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageVIew.image = image
+            let imageData = UIImagePNGRepresentation(image) as NSData?
+            let base64encoding = imageData?.base64EncodedString()
+            socket.emit("images", base64encoding!)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
